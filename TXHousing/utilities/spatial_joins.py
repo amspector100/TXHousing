8a555e083d7026608bd6b26281f896dfc87d9fb2
Asher Spector <amspector100@gmail.com>
2018-08-02 10:51:48 -0400

    Revamped spatial_joins

--- TXHousing/utilities/spatial_joins.py
+++ TXHousing/utilities/spatial_joins.py
@@ -6,7 +6,7 @@ import warnings
 from . import simple
 
 def points_intersect_single_polygon(points, polygon, spatial_index, points_geometry_column = 'geometry',
-                             factor = None, categorical = True, by = 'mean'):
+                             factor = None, categorical = True, by = 'mean', **kwargs):
     """
 
     Given many points and a polygon, finds one of three things. (1) If factor = None, the number of points inside the
@@ -14,36 +14,114 @@ def points_intersect_single_polygon(points, polygon, spatial_index, points_geome
     categorical factor, (3) if factor is not None and categorical = False, the summarized value (mean/median)
     of a factor associated with each point of each point inside the polygon.
 
-    :param points: A GDF with a geometry column
+    :param points: A GDF with a points geometry column
     :param polygon: The polygon to see whether the points are inside.
     :param spatial_index: The spatial index of the points
     :param factor: The factor to average over (if continuous) or subset by (if categorical).
     :param categorical: If True, then the factor should be treated as a categorical variable.
     :param by: If categorical is False, can either summarize using by = 'mean' or by = 'median'
+    :param **kwargs: Kwargs to pass to the "fragment" function in the TXHousing.utilities.simple module. Fragmenting polygons
+        speeds up the computation for all but very small polygons. If you do not want to fragment the polygons (the
+        only reason to do this is speed, it will not affect the results), pass in horiz = 1 and vert = 1 as kwargs.
     :return: float or pandas series
 
-    Note: it might be useful to apply this function to an entire gdf of polygons.
+    Note: it is often useful to apply this function to an entire gdf of polygons.
 
     """
+
     # Get intersections
-    possible_matches_index = list(spatial_index.intersection(polygon.bounds))
-    possible_matches = points.iloc[possible_matches_index]
-    precise_matches_index = possible_matches[points_geometry_column].intersects(polygon)
+    all_precise_matches_indexes = set()
+    grid = simple.fragment(polygon, **kwargs)
+    for grid_piece in grid:
+        possible_matches_index = list(spatial_index.intersection(grid_piece.bounds))
+        possible_matches = points.iloc[possible_matches_index]
+
+        # Prevent weird indexing errors by ignoring empty results
+        if possible_matches.shape[0] == 0:
+            continue
+
+        precise_matches_index = set(possible_matches.index[possible_matches.intersects(grid_piece)].tolist())
+        all_precise_matches_indexes = all_precise_matches_indexes.union(precise_matches_index)
 
     if factor is None:
-        return sum(precise_matches_index)
+        return len(precise_matches_index)
     else:
-        precise_matches = points.loc[precise_matches_index[precise_matches_index.values].index.tolist()]
+        precise_matches = points.loc[all_precise_matches_indexes]
         if categorical == True:
             return precise_matches.groupby(factor)[points_geometry_column].count()
         elif by == 'mean':
-            return precise_matches.groupby(factor)[points_geometry_column].mean()
+            return precise_matches[factor].mean()
         elif by == 'median':
-            return precise_matches.groupby(factor)[points_geometry_column].median()
+            return precise_matches[factor].median()
         else:
             raise ValueError(
             'In points_intersect_polygon call, "by" must either equal "mean" or "median," not "{}"'.format(by))
 
+
+def polygons_intersect_single_polygon(small_polygons, polygon, spatial_index, geometry_column = 'geometry',
+                                      factor = None, categorical = True, account_for_area = True, by = 'mean', **kwargs):
+    """
+
+    Given many polygons (i.e. parcels) and a larger polygon (i.e. county boundary), finds one of three things.
+    (1) If factor = None, the percent area of the large polygon that is covered by the small polygons
+    (2) If factor is not None and categorical = True, the percent area of the large polygon that is covered by the small
+    polygons conditional on the factor
+    (3) if factor is not None and categorical = False, the summarized value (mean/median) of a factor associated with
+    each polygon inside the polygon.
+
+    :param small_polygons: A GDF with a polygon geometry column
+    :param polygon: The polygon to see whether the small_polygons are inside.
+    :param spatial_index: The spatial index of the small_polygons
+    :param factor: The factor to average over (if continuous) or subset by (if categorical).
+    :param categorical: If True, then the factor should be treated as a categorical variable.
+    :param by: If categorical is False, can either summarize using by = 'mean' or by = 'median'
+    :param account_for_area: If categorical is False, by = 'mean', and account_for_area = True, then instead of returning
+        the mean of the factor, this will return the divided dot product of the mean and the area of each small_polygon
+        that intersects the large_polygon.
+    :param **kwargs: Kwargs to pass to the "fragment" function in the TXHousing.utilities.simple module. Fragmenting polygons
+        speeds up the computation for all but very small polygons. If you do not want to fragment the polygons (the
+        only reason to do this is speed, it will not affect the results), pass in horiz = 1 and vert = 1 as kwargs.
+    :return: float (if categorical = False or factor is None) or pandas series
+
+    Note: it is often useful to apply this function to an entire gdf of polygons.
+
+    """
+
+    # Get the indexes of the small polygons which intersect the big polygon
+    precise_matches_indexes = set()
+    grid = simple.fragment(polygon, **kwargs)
+    for grid_piece in grid:
+        possible_matches_index = list(spatial_index.intersection(polygon.bounds))
+        possible_matches = small_polygons.iloc[possible_matches_index]
+
+        # Might speed things up a tiny bit; also might prevent weird indexing errors
+        if possible_matches.shape[0] == 0:
+            continue
+
+        precise_matches_to_add = set(possible_matches.index[possible_matches.intersects(grid_piece)].tolist())
+        precise_matches_indexes = precise_matches_indexes.union(precise_matches_to_add)
+
+    # Calculate actual intersections
+    precise_matches = small_polygons.loc[precise_matches_indexes]
+    precise_matches.loc[:, geometry_column] = precise_matches.loc[:, geometry_column].intersection(polygon)
+    precise_matches['area'] = precise_matches[geometry_column].area
+
+    # Calculate answers
+    if factor is None:
+        return precise_matches['area'].sum()
+    else:
+        if categorical == True:
+            return precise_matches.groupby(factor)['area'].sum().divide(polygon.area)
+        elif by == 'mean':
+            if account_for_area:
+                return precise_matches[factor].multiply(precise_matches['area']).sum()/(polygon.area)
+            else:
+                return precise_matches[factor].mean()
+        elif by == 'median':
+            return precise_matches[factor].median()
+        else:
+            raise ValueError(
+            'In points_intersect_polygon call, "by" must either equal "mean" or "median," not "{}"'.format(by))
 
 
 def points_intersect_multiple_polygons(points_gdf, polygons_gdf, points_spatial_index = None,
@@ -72,7 +150,7 @@ def points_intersect_multiple_polygons(points_gdf, polygons_gdf, points_spatial_
 
     result = {}
     if points_spatial_index is None:
-        points_spatial_index = points_gdf.spatial_index
+        points_spatial_index = points_gdf.sindex
 
     # Create lists of polygons/names from large_polygon_gdf
     polygon_list = polygons_gdf[polygons_geometry_column].values.tolist()
@@ -93,7 +171,7 @@ def points_intersect_multiple_polygons(points_gdf, polygons_gdf, points_spatial_
             precise_intersections_bools = possible_intersections[points_geometry_column].intersects(grid_piece)
             precise_intersections = possible_intersections[precise_intersections_bools].index.tolist()
             for i in precise_intersections:
-                if i in result:
+                if i in result and result[i] != name:
                     warning_count += 1
                 result[i] = name
 
