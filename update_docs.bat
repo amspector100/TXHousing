fb2d26199dd66a67173264fd3c0a1cf637f74d09
Asher Spector <amspector100@gmail.com>
2018-08-07 21:22:09 -0400

    Updating makefile (in progress) and docs

---
+++ update_docs.bat
@@ -0,0 +1,20 @@
+# build docs
+cd docs
+make clean
+make html
+cd ..
+
+# commit and push
+git add -A
+git commit -m "building and pushing docs"
+git push origin master
+
+# switch branches and pull the data we want
+git checkout gh-pages
+rm -rf . # (Clear gh-pages branch)
+touch .nojekyll
+git checkout master docs/build/html
+
+git add -A
+git commit -m "publishing updated docs..."
+git push origin gh-pages
